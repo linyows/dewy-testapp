0cae387ce49fe48a61d6b1a69ff27be15d41c74a
linyows <linyows@gmail.com>
2018-06-19 00:21:45 +0900

    fix starter

--- main.go
+++ main.go
@@ -3,14 +3,55 @@ package main
 import (
 	"fmt"
 	"html"
+	"io"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/lestrrat-go/server-starter/listener"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi, %q\n", html.EscapeString(r.URL.Path))
-}
+const Name string = "dewy-testapp"
+const Version string = "1.1.0"
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3333", nil)
+	var err error
+	var listeners []net.Listener
+
+	if len(os.Args) > 0 {
+		listeners, err = listener.ListenAll()
+	} else {
+		var l net.Listener
+		l, err = net.Listen("tcp", ":3333")
+		listeners = append(listeners, l)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to listen: %s\n", err)
+		os.Exit(1)
+	}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi, %q\n", html.EscapeString(r.URL.Path))
+		io.Copy(w, r.Body)
+	})
+
+	for _, l := range listeners {
+		http.Serve(l, handler)
+	}
+
+	loop := false
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGHUP)
+
+	for loop {
+		select {
+		case <-sigCh:
+			loop = false
+		default:
+			time.Sleep(time.Second)
+		}
+	}
 }
