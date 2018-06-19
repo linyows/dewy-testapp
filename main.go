24001bdf357d5aaacaedbb05da0b58b1f49704f5
linyows <linyows@gmail.com>
2018-06-19 22:24:29 +0900

    update

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 const Name string = "dewy-testapp"
-const Version string = "1.1.0"
+const Version string = "1.2.0"
 
 func main() {
 	var err error
@@ -34,7 +34,7 @@ func main() {
 	}
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi, %q\n", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Yo, %q\n", html.EscapeString(r.URL.Path))
 		io.Copy(w, r.Body)
 	})
 
